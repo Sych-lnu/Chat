96e32c6eea08f63ebac3903dafd53260a641c688
Sych-lnu <79633718+Sych-lnu@users.noreply.github.com>
2022-08-25 11:25:31 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,32 @@
+using System;
+using realtimeChatApp.Hubs;
+public class Startup {
+    public IConfiguration configRoot {
+        get;
+    }
+    public Startup(IConfiguration configuration) {
+        configRoot = configuration;
+    }
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddRazorPages();
+        services.AddSignalR();
+    }
+    public void Configure(WebApplication app, IWebHostEnvironment env) {
+        if (!app.Environment.IsDevelopment()) {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints=>{
+            endpoints.MapRazorPages();
+            
+            endpoints.MapHub<SignalRHub>("/signalr-hub");
+
+        });
+        app.Run();
+    }
+}
